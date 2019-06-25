5b2a6c11b48617902d9434eda68ca99bd94f595b
pmmoncada <pml350@nyu.edu>
2019-06-25 15:54:36 -0600

    test1

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+// sets up my mapbox access token so they can track my usage of their basemap services
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmYTQ2YzBmMTM0ZDAyIn0.owNd_Qa7Sw2neNJbK6zc1A';
+
+
+// instantiate the map
+var map = new mapboxgl.Map({
+  container: 'mapContainer',
+  style: 'mapbox://styles/mapbox/light-v9',
+  center: [-87.197843,14.096704],
+  zoom: 20,
+});
+
+// disable map zoom when using scroll
+map.scrollZoom.disable();
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
+
+//Finish loading base style
+map.on('style.load', function() {
+// set up the geojson as a source in the map
+  map.addSource('test1', {
+     type: 'geojson',
+     data: './Data/test1.geojson',
+   });
+
+// add a custom-styled layer for each Depar
+   map.addLayer({
+     id: 'city-fill',
+     type: 'fill',
+     source: 'test1',
+     paint: {
+         'fill-color': '#088',
+         'fill-opacity': 0.8
+         }
+     }
+   }, 'waterway-label');
