426e1af7c393a98ff8be021536f908472105bd37
pmmoncada <pml350@nyu.edu>
2019-07-01 14:27:57 -0600

    Edits to make the map only of tegucigalpa

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmY
 var map = new mapboxgl.Map({
   container: 'mapContainer',
   style: 'mapbox://styles/mapbox/light-v9',
-  center: [-87.197843,14.096704],
+  center: [-87.210467,14.107138],
   zoom: 15,
 });
 
@@ -17,23 +17,66 @@ var map = new mapboxgl.Map({
 map.addControl(new mapboxgl.NavigationControl());
 
 
+
 //Finish loading base style
 map.on('style.load', function() {
 // set up the geojson as a source in the map
-  map.addSource('test', {
+  map.addSource('tgu', {
      type: 'geojson',
-     data: './Data/test7.geojson',
+     data: './Data/tgu1.geojson',
    });
 
 // add a custom-styled layer for each Depar
    map.addLayer({
-     id: 'city-fill',
+     id: 'lot-fill',
      type: 'fill',
-     source: 'test',
-     layout: {},
+     source: 'tgu',
      paint: {
          'fill-color': '#088',
          'fill-opacity': 0.8
          }
-     }, 'waterway-label');
-});
+     }, 'waterway-label')
+
+
+   // add an empty data source, which we will use to highlight the lot the user is hovering over
+   map.addSource('highlight-feature-lot', {
+     type: 'geojson',
+     data: {
+       type: 'FeatureCollection',
+       features: []
+     }
+   });
+
+   // when the mouse moves, do stuff!
+   map.on('mousemove', function (e) {
+     // query for the features under the mouse, but only in the lots layer
+     var features = map.queryRenderedFeatures(e.point, {
+         layers: ['lot-fill'],
+     });
+
+     // get the first feature from the array of returned features.
+     var lot = features[0]
+
+     if (lot) {  // if there's a lot under the mouse, do stuff
+       map.getCanvas().style.cursor = 'pointer';  // make the cursor a pointer
+
+       // lookup the corresponding description for the each section
+       $('#nombre').text(lot.properties.nombre);
+       $('#barrio').text(lot.properties.barrio);
+       $('#lotUse').text(lot.properties.ZoneCodigo);
+       $('#lot').text(lot.properties.zone);
+       $('#city').text(lot.properties.ciudad);
+
+       // set this lot's polygon feature as the data for the highlight source
+       map.getSource('highlight-feature-lot').setData(lot.geometry);
+     } else {
+      map.getCanvas().style.cursor = 'default'; // make the cursor default
+
+       // reset the highlight source to an empty featurecollection
+       map.getSource('highlight-feature-lot').setData({
+         type: 'FeatureCollection',
+         features: []
+       });
+      }
+     })
+   });
